130dd7cf03f35638e6e073b69db2347a2744c52c
Steve Malsam <stevemalsam@stevemalsam.com>
2015-03-09 21:41:38 -0700

    Added butterknife

--- app/src/main/java/com/stevemalsam/collect_o_tron/MainActivity.java
+++ app/src/main/java/com/stevemalsam/collect_o_tron/MainActivity.java
@@ -1,10 +1,12 @@
 package com.stevemalsam.collect_o_tron;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,5 +16,16 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.listButton)
+    public void toList(Button button) {
+    }
+
+    @OnClick(R.id.rateButton)
+    public void toRate(Button button) {
+
     }
 }
